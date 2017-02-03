3733c24131d8813fb7af3728fe6c8e46343d3427
Kenan Dominic <kenan@Kenans-MacBook-Pro.local>
2017-02-03 01:06:01 -0500

    FINAL2

--- Tumblr/PhotoCell.swift
+++ Tumblr/PhotoCell.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class PhotoCell: UITableViewCell {
 
+    @IBOutlet weak var photoView: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -19,6 +21,14 @@ class PhotoCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell") as! PhotoCell
+        
+        // Configure YourCustomCell using the outlets that you've defined.
+        
+        return cell
     }
 
 }
