69cc927385879aa07d87b02a72675fee710b6517
Kenan Dominic <kenan@Kenans-MacBook-Pro.local>
2017-02-03 01:05:17 -0500

    FINAL

---
+++ Tumblr/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  Tumblr
+//
+//  Created by Kenan Dominic on 2/3/17.
+//  Copyright © 2017 Kenan Dominic. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
